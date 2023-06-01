72a433f1eff567e020c56e63a50e0f9bdda8423b
Gemju Sherpa <gemju.s1@gmail.com>
2023-06-01 23:08:41 +1000

    Model prediction pipeline, Flask Api, Simple Front-end integration

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -2,16 +2,18 @@ import os
 import sys
 
 from dataclasses import dataclass
+
 # from catboost import CatBoostRegressor
 from sklearn.ensemble import (
     AdaBoostRegressor,
     GradientBoostingRegressor,
-    RandomForestRegressor
+    RandomForestRegressor,
 )
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
+
 # from xboost import XGBRegressor
 
 from src.exception import CustomException
@@ -22,14 +24,14 @@ from src.logger import logging
 # data class
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join('artifacts', "model.pkl")
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
 
 
 # Model trainer class
 class ModelTrainer:
     def __init__(self) -> None:
         self.model_trainer_config = ModelTrainerConfig()
-    
+
     def initiate_model_trainer(self, train_arr, test_arr):
         try:
             logging.info("Split training and test dataset for model training")
@@ -37,7 +39,7 @@ class ModelTrainer:
                 train_arr[:, :-1],
                 train_arr[:, -1],
                 test_arr[:, :-1],
-                test_arr[:, -1]
+                test_arr[:, -1],
             )
 
             # models to try training
@@ -48,50 +50,60 @@ class ModelTrainer:
                 "Linear Regression": LinearRegression(),
                 # "XGBRegressor": XGBRegressor(),
                 # "CatBoosting Regressor": CatBoostRegressor(verbose=False),
-                "AdaBoost Regressor": AdaBoostRegressor()
+                "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
-            # params
-            params={
+            # params for hyperparamenter tuning
+            params = {
                 "Decision Tree": {
-                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    "criterion": [
+                        "squared_error",
+                        "friedman_mse",
+                        "absolute_error",
+                        "poisson",
+                    ],
                     # 'splitter':['best','random'],
                     # 'max_features':['sqrt','log2'],
                 },
-                "Random Forest":{
+                "Random Forest": {
                     # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                 
                     # 'max_features':['sqrt','log2',None],
-                    'n_estimators': [8,16,32,64,128,256]
+                    "n_estimators": [8, 16, 32, 64, 128, 256]
                 },
-                "Gradient Boosting":{
+                "Gradient Boosting": {
                     # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
-                    'learning_rate':[.1,.01,.05,.001],
-                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "subsample": [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
                     # 'criterion':['squared_error', 'friedman_mse'],
                     # 'max_features':['auto','sqrt','log2'],
-                    'n_estimators': [8,16,32,64,128,256]
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
                 },
-                "Linear Regression":{},
-                "XGBRegressor":{
-                    'learning_rate':[.1,.01,.05,.001],
-                    'n_estimators': [8,16,32,64,128,256]
+                "Linear Regression": {},
+                "XGBRegressor": {
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
                 },
-                "CatBoosting Regressor":{
-                    'depth': [6,8,10],
-                    'learning_rate': [0.01, 0.05, 0.1],
-                    'iterations': [30, 50, 100]
+                "CatBoosting Regressor": {
+                    "depth": [6, 8, 10],
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "iterations": [30, 50, 100],
                 },
-                "AdaBoost Regressor":{
-                    'learning_rate':[.1,.01,0.5,.001],
+                "AdaBoost Regressor": {
+                    "learning_rate": [0.1, 0.01, 0.5, 0.001],
                     # 'loss':['linear','square','exponential'],
-                    'n_estimators': [8,16,32,64,128,256]
-                }
-                
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
             }
 
             # evaluate the models
-            model_report:dict=evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, params=params)
+            model_report: dict = evaluate_models(
+                X_train=X_train,
+                y_train=y_train,
+                X_test=X_test,
+                y_test=y_test,
+                models=models,
+                params=params,
+            )
 
             # get the best model from dict
             best_model_score = max(sorted(model_report.values()))
@@ -105,13 +117,13 @@ class ModelTrainer:
             # Check if model is actually best, if not raise warning
             if best_model_score < 0.6:
                 raise CustomException("No best model found")
-            
+
             logging.info("Best models both on test and trainig")
 
             # save model
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
-                obj=best_model
+                obj=best_model,
             )
 
             # prediction on best model
@@ -121,5 +133,3 @@ class ModelTrainer:
             return r2_scores
         except Exception as e:
             raise CustomException(e, sys)
-    
-
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import pandas as pd
 import pickle
+import dill
 
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
@@ -11,6 +12,8 @@ from src.exception import CustomException
 """
 Save object on specified file path
 """
+
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -22,9 +25,12 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
+
 """
 Evaluate the model performance
 """
+
+
 def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
@@ -50,8 +56,14 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params):
             report[list(models.keys())[i]] = test_model_score
 
             return report
-        
+
     except Exception as e:
         raise CustomException(e, sys)
-    
-    
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
